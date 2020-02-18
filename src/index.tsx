834e868a67321e3be9b994abdcc997285b082c30
UndyingSugimoto <kouji36000@gmail.com>
2020-02-19 03:09:50 +0900

    ゴミを削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    request: operation => {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${
+            process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+          }`
+        }
+      });
+    }
+  });
+
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`
+
+client
+  .query({
+    query
+  })
+  .then(result => console.log(result));
