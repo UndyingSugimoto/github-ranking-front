5c86afad5b7fbb4cf0591b00c4135ac6cd121ad9
UndyingSugimoto <kouji36000@gmail.com>
2020-02-22 22:28:38 +0900

    間違えて消したファイルを復元

---
+++ src/util/AppClient.ts
@@ -0,0 +1,12 @@
+import ApolloClient from "apollo-boost";
+
+export default new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+      }
+    });
+  }
+});
