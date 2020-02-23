85b6fd97cbe3abcbc5016db2202b8923d905e9bd
UndyingSugimoto <kouji36000@gmail.com>
2020-02-24 00:07:32 +0900

    コンポーネントのテスト完了

--- src/components/parts/UserTierCardWrapper.test.tsx
+++ src/components/parts/UserTierCardWrapper.test.tsx
@@ -1,15 +1,74 @@
-// import { Alert } from "./Alert";
-// import { shallow } from "enzyme";
-// import React from "react";
+import { mount } from "enzyme";
+import React from "react";
+import { UserTierCardWrapper } from "./UserTierCardWrapper";
+import { UserTierCard } from "./UserTierCard";
+import {
+  CHALLENGER,
+  MASTER,
+  DIAMOND,
+  PRATINUM,
+  GOLD,
+  SILVER,
+  BRONZE
+} from "../../const/Tier";
+import bronze from "../../static/bronze.png";
+import silver from "../../static/silver.png";
+import gold from "../../static/gold.png";
+import pratinum from "../../static/pratinum.png";
+import diamond from "../../static/diamond.png";
+import master from "../../static/master.png";
+import challenger from "../../static/challenger.png";
 
-// describe("UserTierCardWrapper", () => {
-//   it("レンダリングの確認", () => {});
-//   it("propsをちゃんと渡せてることの確認", () => {});
-//   it("Challengerの場合", () => {});
-//   it("Masterの場合", () => {});
-//   it("Diamondの場合", () => {});
-//   it("Pratinumの場合", () => {});
-//   it("Goldの場合", () => {});
-//   it("Silverの場合", () => {});
-//   it("それ以外の場合", () => {});
-// });
+describe("UserTierCardWrapper", () => {
+  it("レンダリングの確認", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={"tier"} score={0} />);
+    expect(wrapper.find(UserTierCard).length).toBe(1);
+  });
+  it("propsをちゃんと渡せてることの確認", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={CHALLENGER} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("score")).toBe(0);
+  });
+  it("Challengerの場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={CHALLENGER} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Top 5%");
+    expect(card.prop("imageSrc")).toBe(challenger);
+  });
+  it("Masterの場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={MASTER} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Top 10%");
+    expect(card.prop("imageSrc")).toBe(master);
+  });
+  it("Diamondの場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={DIAMOND} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Top 15%");
+    expect(card.prop("imageSrc")).toBe(diamond);
+  });
+  it("Pratinumの場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={PRATINUM} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Top 20%");
+    expect(card.prop("imageSrc")).toBe(pratinum);
+  });
+  it("Goldの場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={GOLD} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Top 30%");
+    expect(card.prop("imageSrc")).toBe(gold);
+  });
+  it("Silverの場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={SILVER} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Top 40%");
+    expect(card.prop("imageSrc")).toBe(silver);
+  });
+  it("それ以外の場合", () => {
+    const wrapper = mount(<UserTierCardWrapper tier={BRONZE} score={0} />);
+    const card = wrapper.find(UserTierCard);
+    expect(card.prop("tierAbout")).toBe("Under 40%");
+    expect(card.prop("imageSrc")).toBe(bronze);
+  });
+});
--- src/components/parts/UserUpdateCard.test.tsx
+++ src/components/parts/UserUpdateCard.test.tsx
@@ -1,9 +1,31 @@
-// import { Alert } from "./Alert";
-// import { shallow } from "enzyme";
-// import React from "react";
+import { Alert } from "./Alert";
+import { shallow, mount } from "enzyme";
+import React from "react";
+import { UserUpdateCard } from "./UserUpdateCard";
+import { Card, Button } from "@material-ui/core";
 
-// describe("UserUpdateCard", () => {
-//   it("レンダリングの確認", () => {});
-//   it("propsをちゃんと渡せてることの確認", () => {});
-//   it("クリック時にコールバックが呼ばれることを確認", () => {});
-// });
+const date = new Date();
+const testMock = jest.fn();
+
+describe("UserUpdateCard", () => {
+  it("レンダリングの確認", () => {
+    const wrapper = mount(
+      <UserUpdateCard lastupdateDate={date} onClickUpdateButton={testMock} />
+    );
+    expect(wrapper.find(Card).length).toBe(1);
+  });
+  it("propsをちゃんと渡せてることの確認", () => {
+    const wrapper = mount(
+      <UserUpdateCard lastupdateDate={date} onClickUpdateButton={testMock} />
+    );
+    expect(wrapper.prop("lastupdateDate")).toBe(date);
+    expect(wrapper.prop("onClickUpdateButton")).toBe(testMock);
+  });
+  it("クリック時にコールバックが実行されることを確認", () => {
+    const wrapper = mount(
+      <UserUpdateCard lastupdateDate={date} onClickUpdateButton={testMock} />
+    );
+    wrapper.find(Button).simulate("click");
+    expect(testMock).toHaveBeenCalled();
+  });
+});
