8d5ddf0815b27162170d22d94b5efdc0d578e240
UndyingSugimoto <kouji36000@gmail.com>
2020-02-23 22:06:27 +0900

    テストを追加

--- src/components/parts/RankingList.test.tsx
+++ src/components/parts/RankingList.test.tsx
@@ -1,8 +1,33 @@
-// import { Alert } from "./Alert";
-// import { shallow } from "enzyme";
-// import React from "react";
+import { mount } from "enzyme";
+import React from "react";
+import { RankingList } from "./RankingList";
+import { RanksByLanguage } from "../../external/data/RanksByLanguage";
+import { Grid } from "@material-ui/core";
 
-// describe("RankingList", () => {
-//   it("レンダリングの確認", () => {});
-//   it("propsをちゃんと渡せてることの確認", () => {});
-// });
+const ranksByLanguage: RanksByLanguage = {
+  language: "language",
+  userInfomations: []
+};
+const testMock = jest.fn();
+
+describe("RankingList", () => {
+  it("レンダリングの確認", () => {
+    const wrapper = mount(
+      <RankingList
+        ranksByLanguage={ranksByLanguage}
+        itemClickCallback={testMock}
+      />
+    );
+    expect(wrapper.find(Grid).length).toBe(1);
+  });
+  it("propsをちゃんと渡せてることの確認", () => {
+    const wrapper = mount(
+      <RankingList
+        ranksByLanguage={ranksByLanguage}
+        itemClickCallback={testMock}
+      />
+    );
+    expect(wrapper.prop("ranksByLanguage").language).toBe("language");
+    expect(wrapper.prop("itemClickCallback")).toBe(testMock);
+  });
+});
